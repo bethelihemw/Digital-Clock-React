8eb24fc550d98b859475e0e7055b3f263fdb2e82
Bethelihem Wondimneh <wondimnehbethelihem@gmail.com>
2025-07-12 01:52:52 +0300

    last commit

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,10 +1,34 @@
 import React, {useState, useEffect} from "react"
 
 function DigitalClock(){
+
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+        const interval = setInterval(()=> {
+            setTime(new Date());
+        }, 1000);
+        return()=>{
+            clearInterval(interval);
+        }
+    }, [])
+    function formatTime(){
+        let hrs = time.getHours()
+        const min = time.getMinutes()
+        const sec = time.getSeconds()
+        const mr = hrs >= 12 ? "PM" : "AM";
+        hrs = hrs % 12 || 12;
+
+        return `${padZero(hrs)}:${padZero(min)}:${padZero(sec)} ${mr}`
+    }
+
+    function padZero(num){
+        
+        return (num < 10 ? "0" : "") + num;
+    }
      return(
         <div className="clock-container">
             <div className="clock">
-                <span>00:00:00</span>
+                <span>{formatTime()}</span>
             </div>
         </div>
      )
